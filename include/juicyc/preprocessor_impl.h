48529fde23f1b9e71119c96ba44935c3b4b03c19
ZJUnONE <zjufenghang@gmail.com>
2019-05-23 13:22:58 +0800

    Modify some problems and add include function

--- include/juicyc/preprocessor_impl.h
+++ include/juicyc/preprocessor_impl.h
@@ -1,16 +1,16 @@
 //Created by FENG, H
-#ifndef JUICYC_PREPROCESSOR_IMPL_H_
-#define JUICYC_PREPROCESSOR_IMPL_H_
+#ifndef JUICYC_PreprocessorImpl_H_
+#define JUICYC_PreprocessorImpl_H_
 
 #include "preprocessor.h" 
 using namespace std;
-namespace juicy {
+namespace juicyc {
 
 //pp_impl class
 //create a pp_impl class
 //specify the construction function with one parameter
 //with construct function pass parameters about input files(CompilerOption& opt)
-class Preprocessor_impl: public Preprocessor
+class PreprocessorImpl: public Preprocessor
 {
 public:
 	//public inherited
@@ -22,16 +22,16 @@ public:
 	//task scheduler
 	void push(std::string& file);
 	//added 
-	//noted, from opt -> input_files_names
-	//output_files -> opt
-	vector<string> input_files_names;
-	vector<string> output_files;
-	Preprocessor_impl(CompilerOption& opt):
-		input_files_names(opt.files)
+	//noted, from opt -> input_files_names_
+	//output_files_ -> opt
+	PreprocessorImpl(CompilerOption& opt):
+		input_files_names_(opt.files)
 	{}
-	~Preprocessor_impl() {}
-	void process_define();
-	void process_include();
+	~PreprocessorImpl() {}
+	Status Run();
+protected:
+	vector<string> input_files_names_;
+	vector<string> output_files_;
 };
 
 }
